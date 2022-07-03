cd848646c366bd08a4527c73cdd000343111da52
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-07-03 11:23:34 +0530

    Implemented Router Event, can see all even in console tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   //benefit of using this is that 
   //if u copy the same in any oher browser tab, the route will be displayed correctly
   //forRoot -> an array of routes
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes,{useHash:true, enableTracing : true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
